2054396d61533a3913d10ce951d8c23681c9075f
Melody Sofia Eroshevich <se@cloudeteer.de>
2025-03-05 22:11:25 +0100

    Add network and security groups

---
+++ cloud/stackit/first-test/terraform/network.tf
@@ -0,0 +1,30 @@
+locals {
+  cidr_ipv4 = "10.0.0.0/16"
+  cidr_ipv6 = "fddc:d8c8:d914::/48"
+}
+
+resource "stackit_network" "melo_test_network" {
+  project_id  = data.stackit_resourcemanager_project.melo_test.project_id
+  name        = "melo-test-network"
+  ipv4_prefix = local.cidr_ipv4
+  ipv6_prefix = local.cidr_ipv6
+
+  labels = local.labels
+}
+
+resource "stackit_network_interface" "melo_test_nic" {
+  name               = "melo-test-nic"
+  project_id         = data.stackit_resourcemanager_project.melo_test.project_id
+  network_id         = stackit_network.melo_test_network.network_id
+  security           = true
+  security_group_ids = [stackit_security_group.melo_test_sec_group.security_group_id]
+
+  labels = local.labels
+}
+
+resource "stackit_public_ip" "melo_test_pip" {
+  project_id           = data.stackit_resourcemanager_project.melo_test.project_id
+  network_interface_id = stackit_network_interface.melo_test_nic.network_interface_id
+
+  labels = local.labels
+}
---
+++ cloud/stackit/first-test/terraform/security_group.tf
@@ -0,0 +1,54 @@
+resource "stackit_security_group" "melo_test_sec_group" {
+  project_id = data.stackit_resourcemanager_project.melo_test.project_id
+  name       = "melo-test-security-group"
+  stateful   = true
+
+  labels = local.labels
+}
+
+locals {
+  rules = {
+    SSH   = 22
+    HTTPS = 443
+  }
+}
+
+resource "stackit_security_group_rule" "melo_test_rule_v4" {
+  for_each = local.rules
+
+  project_id        = data.stackit_resourcemanager_project.melo_test.project_id
+  security_group_id = stackit_security_group.melo_test_sec_group.security_group_id
+  description       = "IPv4, ingress, ${each.key}"
+  direction         = "ingress"
+  # plan validation spell check missing (case sensitive)
+  ether_type = "IPv4"
+
+  protocol = {
+    name = "tcp"
+  }
+
+  port_range = {
+    min = each.value
+    max = each.value
+  }
+}
+
+resource "stackit_security_group_rule" "melo_test_rule_v6" {
+  for_each = local.rules
+
+  project_id        = data.stackit_resourcemanager_project.melo_test.project_id
+  security_group_id = stackit_security_group.melo_test_sec_group.security_group_id
+  description       = "IPv6, ingress, ${each.key}"
+  direction         = "ingress"
+  # plan validation spell check missing (case sensitive)
+  ether_type = "IPv6"
+
+  protocol = {
+    name = "tcp"
+  }
+
+  port_range = {
+    min = each.value
+    max = each.value
+  }
+}
